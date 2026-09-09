261f25053f0968380a06c5d95b64dbb1fa0d450c
Hariom <hariomsingh945@gmail.com>
2026-09-09 10:35:23 +0530

    added

--- project/main.tf
+++ project/main.tf
@@ -1,6 +1,6 @@
 resource "google_project" "create_project" {
-  name       = "test-project-id23"
-  project_id = "test-project-id23"
+  name       = "test-project-id2345"
+  project_id = "test-project-id2345"
   billing_account = "01C41B-0A7288-4C2013"
   deletion_policy = "DELETE"
 }
