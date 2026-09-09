2a2e247f74593730bec008bcc3a1518228aa564c
Hariom <hariomsingh945@gmail.com>
2026-09-09 17:53:55 +0530

    delete

--- project/main.tf
+++ project/main.tf
@@ -1,6 +1,6 @@
-resource "google_project" "create_project" {
-  name       = "test-project-id23456"
-  project_id = "test-project-id23456"
-  billing_account = "01C41B-0A7288-4C2013"
-  deletion_policy = "DELETE"
-}
+# resource "google_project" "create_project" {
+#   name       = "test-project-id23456"
+#   project_id = "test-project-id23456"
+#   billing_account = "01C41B-0A7288-4C2013"
+#   deletion_policy = "DELETE"
+# }
